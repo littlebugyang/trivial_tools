bb3580ed51b643ecf91d54a74044c3f6310c4150
littlebugyang <im.lby@qq.com>
2021-04-15 23:01:04 +0800

    Complete the function with lots of pitfall.

--- auto_front_matter.js
+++ auto_front_matter.js
@@ -1,18 +1,45 @@
-const { promisify } = require('util');
-const { resolve } = require('path');
-const fs = require('fs');
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
+/*
+ * @Date: 2021-04-15 21:57:31
+ * @LastEditors: lby
+ */
+const { promisify } = require('util')
+const { resolve, parse, relative } = require('path')
+const fs = require('fs')
+const readdir = promisify(fs.readdir)
+const stat = promisify(fs.stat)
+
+const baseDir = ''
+const targetDir = ''
 
 async function getFiles(dir) {
-  const subdirs = await readdir(dir);
-  const files = await Promise.all(subdirs.map(async (subdir) => {
-    const res = resolve(dir, subdir);
-    return (await stat(res)).isDirectory() ? getFiles(res) : res;
-  }));
-  return files.reduce((a, f) => a.concat(f), []);
+  const subdirs = await readdir(dir)
+  const files = await Promise.all(
+    subdirs.map(async (subdir) => {
+      const res = resolve(dir, subdir)
+      return (await stat(res)).isDirectory() ? getFiles(res) : res
+    })
+  )
+  return files.reduce((a, f) => a.concat(f), [])
 }
 
-getFiles("C:/Users/lby/syncthing_files/cross_platform/study/notes")
-  .then(files => console.log(files))
-  .catch(e => console.error(e));
+function addFrontMatter(paths) {
+  for (let i = 0; i < paths.length; ++i) {
+    const subtractBase = relative(baseDir, paths[i])
+    const parsedPath = parse(paths[i])
+
+    if (parsedPath.ext === '.md') {
+      const article = fs.readFileSync(paths[i], { encoding: 'utf8' })
+      const targetPath = resolve(targetDir, subtractBase)
+      console.log(targetPath)
+      fs.writeFileSync(
+        targetPath,
+        `---\ntitle: ${parsedPath.name}\ncategories:\n---\n${article}`
+      )
+    }
+  }
+}
+
+getFiles(baseDir)
+  .then(addFrontMatter)
+  .catch((e) => console.error(e))
+console.log('Done. ')
